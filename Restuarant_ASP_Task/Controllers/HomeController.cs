b1f00f7ebce0bcaf8dcae1cdb347c42a551145c5
Xeli_hx11@itstep.edu.az <Xeli_hx11@itstep.edu.az>
2022-04-19 12:34:37 +0400

    Add project files.

---
+++ Restuarant_ASP_Task/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restuarant_ASP_Task.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restuarant_ASP_Task.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IRepository _repository;
+        public HomeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _repository.GetAll();
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            var model = _repository.GetAll();
+            return View(model);
+        }
+
+        public IActionResult Special_Dishes()
+        {
+            var model = _repository.GetAll();
+            return View(model);
+
+        }
+
+        public IActionResult Menu()
+        {
+            var model = _repository.GetAll();
+            return View(model);
+        }
+
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+        public IActionResult Reservation()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Restuarant_ASP_Task/Services/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Restuarant_ASP_Task.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restuarant_ASP_Task.Services
+{
+    public interface IRepository
+    {
+        void Add(Meal item);
+        void Delete(int id);
+        void Update(Meal item);
+        Meal Get(int id);
+        IEnumerable<Meal> GetAll();
+    }
+}
